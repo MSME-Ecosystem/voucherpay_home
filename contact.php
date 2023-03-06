997b08162de841a941528a61293a7cf261dd6fb5
ayanda-boluwatife <ayandastephen1000@gmail.com>
2023-03-06 10:37:32 +0100

    changing the file back to .php from .html

---
+++ contact.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+    <link rel="icon" type="image/png" href="assets/images/Voucher Pay+==.png" />
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
+    <meta charSet="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <title>Online - Voucher</title>
+    <meta name="next-head-count" content="3" />
+    
+    <link rel="stylesheet" href="assets/static/css/9c68429a98c3f122.css" data-n-g="" />
+    
+    <noscript data-n-css=""></noscript>
+    <script defer="" nomodule="" src="assets/static/chunks/polyfills-5cd94c89d3acac5f.js"></script>
+    <script src="assets/static/chunks/webpack-4cd07d08d8399fab.js" defer=""></script>
+    <script src="assets/static/chunks/framework-8957c350a55da097.js" defer=""></script>
+    <script src="assets/static/chunks/main-f65253c7c555ee10.js" defer=""></script>
+    <script src="assets/static/chunks/pages/_app-b998c70e5b0bdd1c.js" defer=""></script>
+    <script src="assets/static/chunks/669-9fda595c9d9e08bb.js" defer=""></script>
+    <script src="assets/static/chunks/199-9daa5e608690e290.js" defer=""></script>
+    <script src="assets/static/chunks/423-37ab5811a42b3b04.js" defer=""></script>
+    <script src="assets/static/chunks/399-9bd747effbda85ea.js" defer=""></script>
+    
+    
+</head>
+
+<body>
+    <?php include('./nav-bar.html') ?>
+       <div class="page-title-area item-bg2">
+            <div class="container">
+                <div class="page-title-content">
+                    <h2>Contact</h2>
+                    <p>If you have an idea, we would love to hear about it.</p>
+                </div>
+            </div>
+        </div>
+        <div class="contact-area ptb-70">
+            <div class="container">
+                <div class="section-title">
+                    <h2>Drop us a message for further enquiries</h2>
+                    <div class="bar"></div>
+                    <p>Do you have any enquiries regarding your voucherpay account?  We would love to hear them.</p>
+                </div>
+                <div class="row">
+                    <div class="col-lg-5 col-md-12">
+                        <div class="contact-info">
+                            <ul>
+                                <li>
+                                    <div class="icon">
+                                        <i class="fas fa-map-marker-alt"></i>
+                                    </div>
+                                    <span>Address</span>
+                                    <span>No 8, Stadium Road Ilorin, Kwara State.</span> 
+                                </li>
+                                <li>
+                                    <div class="icon"><i class="fas fa-envelope"></i></div><span>Email</span>
+                                    <a href="../cdn-cgi/l/email-protection.html#5b32353d34333a322b3e291b333a322b3e2975383436">
+                                        <span class="__cf_email__" data-cfemail="640d0a020b0c050d140116240c050d1401164a070b09">Support@voucherpay.online</span>
+                                    </a>
+                                <li>
+                                    <div class="icon"><i class="fas fa-phone-volume"></i></div><span>Phone</span>
+                                    <a href="tel:+ (234) 8184432979">+ (234) 8184432979</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-7 col-md-12">
+                        <div class="contact-form">
+                            <form id="contactForm">
+                                <div class="row">
+                                    <div class="col-lg-6 col-md-6">
+                                        <div class="form-group"><input type="text" name="name" placeholder="Name"
+                                                class="form-control" value="" />
+                                            <div class="invalid-feedback" style="display:block"></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-6">
+                                        <div class="form-group"><input type="email" name="email" placeholder="Email"
+                                                class="form-control" value="" />
+                                            <div class="invalid-feedback" style="display:block"></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-6">
+                                        <div class="form-group"><input type="text" name="number" placeholder="Phone"
+                                                class="form-control" value="" />
+                                            <div class="invalid-feedback" style="display:block"></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-6">
+                                        <div class="form-group"><input type="text" name="subject" placeholder="Subject"
+                                                class="form-control" value="" />
+                                            <div class="invalid-feedback" style="display:block"></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12 col-md-12">
+                                        <div class="form-group"><textarea name="text" cols="30" rows="5"
+                                                placeholder="Write your message..." class="form-control"></textarea>
+                                            <div class="invalid-feedback" style="display:block"></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12 col-sm-12">
+                                        <button type="submit" class="btn btn-primary">SendMessage</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bg-map"><img src="assets/images/bg-map.png" alt="image" /></div>
+        </div>
+        <div class="account-create-area">
+            <div class="container account-create-content">
+                <div class="row align-items-center">
+                    <div class="col-lg-8">
+                        <h2>Apply for an account in minutes</h2>
+                        <p>Get your Haiper account today!</p>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="create-account"><a class="btn btn-primary" href="../sign-up/index.html">Get Your
+                                Account</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php include('./footer.html') ?>
+        
+        <div class="back-to-top show-back-to-top"></div>
+    </div>
+   
+</body>
+
+</html>
---
+++ pricing.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+    <link rel="icon" type="image/png" href="assets/images/Voucher Pay+==.png" />
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
+    <meta charSet="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <title>Online - Voucher</title>
+    <meta name="next-head-count" content="3" />
+    <link rel="preload" href="assets/static/css/9c68429a98c3f122.css" as="style" />
+    <link rel="stylesheet" href="assets/static/css/9c68429a98c3f122.css" data-n-g="" /><noscript
+        data-n-css=""></noscript>
+    <script defer="" nomodule="" src="assets/static/chunks/polyfills-5cd94c89d3acac5f.js"></script>
+    <script src="assets/static/chunks/webpack-4cd07d08d8399fab.js" defer=""></script>
+    <script src="assets/static/chunks/framework-8957c350a55da097.js" defer=""></script>
+    <script src="assets/static/chunks/main-f65253c7c555ee10.js" defer=""></script>
+    <script src="assets/static/chunks/pages/_app-b998c70e5b0bdd1c.js" defer=""></script>
+    <script src="assets/static/chunks/423-37ab5811a42b3b04.js" defer=""></script>
+    <script src="assets/static/chunks/399-9bd747effbda85ea.js" defer=""></script>
+    <script src="assets/static/chunks/pages/pricing-9332e27b5b7de889.js" defer=""></script> 
+   
+</head>
+
+<body>
+    <div id="__next">
+       
+    <?php include('./nav-bar.html') ?>
+         <div class="page-title-area item-bg4">
+            <div class="container">
+                <div class="page-title-content">
+                    <h2>Transparent Pricing</h2>
+                    <p>Borderless account pricing</p>
+                </div>
+            </div>
+        </div>
+        <div class="pricing-area pt-70 pb-50">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-pricing-box">
+                            <div class="pricing-header">
+                                <h3>Basic</h3>
+                                <p>Get your business up and running</p>
+                            </div>
+                            <div class="price"><span class="symbol">$</span> 0 <span>/ monthly</span></div>
+                            <ul class="pricing-features">
+                                <li>100 outgoing transfers/mo<i class="fas fa-check"></i></li>
+                                <li>5 Luvion Users<i class="fas fa-check"></i></li>
+                                <li>2 Business Mastercards<i class="fas fa-check"></i></li>
+                                <li>Premium Support<i class="fa fa-ban"></i></li>
+                                <li>International payments<i class="fa fa-ban"></i></li>
+                                <li>Accounting integrations<i class="fa fa-ban"></i></li>
+                            </ul>
+                            <div class="buy-btn"><a class="btn btn-primary" href="index.html#">Choose this plan</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-pricing-box active">
+                            <div class="pricing-header">
+                                <h3>Advanced</h3>
+                                <p>Get your business up and running</p>
+                            </div>
+                            <div class="price"><span class="symbol">$</span> 20 <span>/ monthly</span></div>
+                            <ul class="pricing-features">
+                                <li>100 outgoing transfers/mo<i class="fas fa-check"></i></li>
+                                <li>5 Luvion Users<i class="fas fa-check"></i></li>
+                                <li>2 Business Mastercards<i class="fas fa-check"></i></li>
+                                <li>Premium Support<i class="fas fa-check"></i></li>
+                                <li>International payments<i class="fas fa-check"></i></li>
+                                <li>Accounting integrations<i class="fa fa-ban"></i></li>
+                            </ul>
+                            <div class="buy-btn"><a class="btn btn-primary" href="index.html#">Choose this plan</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 offset-lg-0 offset-md-3">
+                        <div class="single-pricing-box">
+                            <div class="pricing-header">
+                                <h3>Premium</h3>
+                                <p>Get your business up and running</p>
+                            </div>
+                            <div class="price"><span class="symbol">$</span> 24 <span>/ monthly</span></div>
+                            <ul class="pricing-features">
+                                <li>100 outgoing transfers/mo<i class="fas fa-check"></i></li>
+                                <li>5 Luvion Users<i class="fas fa-check"></i></li>
+                                <li>2 Business Mastercards<i class="fas fa-check"></i></li>
+                                <li>Premium Support<i class="fas fa-check"></i></li>
+                                <li>International payments<i class="fas fa-check"></i></li>
+                                <li>Accounting integrations<i class="fas fa-check"></i></li>
+                            </ul>
+                            <div class="buy-btn"><a class="btn btn-primary" href="index.html#">Choose this plan</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="account-create-area">
+            <div class="container account-create-content">
+                <div class="row align-items-center">
+                    <div class="col-lg-8">
+                        <h2>Apply for an account in minutes</h2>
+                        <p>Get your Haiper account today!</p>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="create-account"><a class="btn btn-primary" href="assets/sign-up/index.html">Get Your
+                                Account</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php include('./footer.html') ?>
+        <div class="back-to-top show-back-to-top"></div>
+    </div>
+ 
+</body>
+
+</html>
